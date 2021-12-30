1f431dac978f91e0d34a306394b1042887b9bc2a
taruvar-mittal <mittaltaruvar@gmail.com>
2021-12-30 21:43:05 +0530

    styles and errors

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const path = require('path');
 const mongoose = require('mongoose');
 const Campground = require('./models/campground');
 const methodOverride = require('method-override');
+const ejsMate = require('ejs-mate');
+const catchAsync = require('./utils/catchAsync');
+const ExpressError = require('./utils/ExpressError');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp-v2',{
     useNewUrlParser : true,
@@ -17,6 +20,7 @@ db.once('open', () => {
     console.log('Mongo database connected');
 })
 
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'));
 
@@ -27,40 +31,51 @@ app.get('/', (req,res) => {
     res.render('Home');
 })
 
-app.get('/campgrounds', async (req,res) => {
+app.get('/campgrounds', catchAsync( async (req,res) => {
    const campgrounds = await Campground.find({});
    res.render('./campgrounds/index', { campgrounds });
-})
+}))
 
 app.get('/campgrounds/new', (req,res) => {
     res.render('./campgrounds/new');
 })
 
-app.post('/campgrounds', async (req,res) => {
-    const campground = new Campground(req.body.campground);
-    await campground.save();
-    res.redirect(`/campgrounds/${campground._id}`);
-})
+app.post('/campgrounds', catchAsync( async (req,res,next) => { 
+    if(!req.body.campground) throw new ExpressError('Invalid campground details!', 400);
+        const campground = new Campground(req.body.campground);
+        await campground.save();
+        res.redirect(`/campgrounds/${campground._id}`);
+}))
 
-app.get('/campgrounds/:id', async (req,res) => {
+app.get('/campgrounds/:id', catchAsync( async (req,res) => {
     const campground = await Campground.findById(req.params.id);
     res.render('./campgrounds/show', { campground });
-})
+}))
 
-app.get('/campgrounds/:id/edit', async (req,res) => {
+app.get('/campgrounds/:id/edit', catchAsync( async (req,res) => {
     const campground = await Campground.findById(req.params.id);
     res.render('./campgrounds/edit',{ campground });
-})
+}))
 
-app.put('/campgrounds/:id', async (req, res) => {
+app.put('/campgrounds/:id',catchAsync(  async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
     res.redirect(`/campgrounds/${campground._id}`)
-});
+}))
 
-app.delete('/campgrounds/:id', async (req,res) => {
+app.delete('/campgrounds/:id', catchAsync( async (req,res) => {
     const campground = await Campground.findByIdAndDelete(req.params.id);
     res.redirect('/campgrounds');
+}))
+
+app.all('*', (req,res,next) => {
+    next(new ExpressError('Page not Found',404));
+})
+
+app.use((err,req,res,next) => {
+    const { statusCode = 500 } = err;
+    if(!err.message) err.message = 'Something went wrong!';
+    res.status(statusCode).render('error', { err } );
 })
 
 app.listen(3000,() => {
--- seeds/index.js
+++ seeds/index.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 const Campground = require('../models/campground')
 const cities = require('./cities')
 const { places, descriptors } = require('./seedHelpers')
+const axios = require('axios')
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp-v2',{
     useNewUrlParser : true,
@@ -16,13 +17,33 @@ db.once('open', () => {
 
 const sample = array => array[Math.floor(Math.random()*array.length)];
 
+
+async function seedImg() {
+    try {
+      const resp = await axios.get('https://api.unsplash.com/photos/random', {
+        params: {
+          client_id: 'Imvi7PbQVIRLCyIkwYLGuF9GAP6Ui3S3nz75aSbkpXc',
+          collections: 1114848,
+        },
+      })
+      return resp.data.urls.small
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
 const seedDB = async () => {
     await Campground.deleteMany();
-    for(let i=0;i<50;i++){
+    
+    for(let i=0;i<20;i++){
         const random1000 = Math.floor(Math.random()*1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             title : `${sample(descriptors)} ${sample(places)}`,
-            location : `${cities[random1000].city}, ${cities[random1000].state}`
+            location : `${cities[random1000].city}, ${cities[random1000].state}`,
+            image : await seedImg(),
+            price,
+            description : 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione nostrum dolores facere! Dolore eveniet molestiae omnis laborum adipisci tenetur, reprehenderit corporis aliquid, excepturi voluptatibus alias deserunt ab debitis ullam quaerat ipsa cupiditate sunt, aperiam veritatis. Iusto dignissimos sint voluptate numquam, sed inventore, totam expedita possimus tempora perspiciatis accusantium, sequi consequatur!'
         })
         await camp.save();
     }
@@ -32,4 +53,7 @@ const seedDB = async () => {
 seedDB().then(() => {
     mongoose.connection.close();
 })
+
+
+
 
