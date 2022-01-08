70f9839022b6cb445c54a0d93cb11fa691f0d0ab
taruvar-mittal <mittaltaruvar@gmail.com>
2022-01-08 21:54:21 +0530

    deploy

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const flash = require('connect-flash');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
+const MongoDbStore = require('connect-mongo');
 // const helmet = require('helmet');
 
 const expressMongoSanitize = require('express-mongo-sanitize');
@@ -20,8 +21,13 @@ const expressMongoSanitize = require('express-mongo-sanitize');
 const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
+ const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/yelp-camp-v2";
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp-v2',{
+ //process.env.DB_URL
+//mongodb://localhost:27017/yelp-camp-v2
+//dbUrl
+
+mongoose.connect(dbUrl,{
     useNewUrlParser : true,
     useUnifiedTopology : true,
 });
@@ -44,8 +50,21 @@ app.use(methodOverride('_method'));
 app.use(expressMongoSanitize());
 // app.use(helmet());
 
+const secret = process.env.SECRET || 'thisshouldbemysecret';
+
+const store = new MongoStore({
+    url : dbUrl,
+    secret,
+    touchAfter : 24*60*60
+})
+
+store.on("error", function(e) {
+    console.log("session store error");
+})
+
 const sessionConfig = {
-    secret : 'thisshouldbemysecret',
+    store,
+    secret,
     resave : false,
     saveUninitialized : true,
     cookie : {
