e2b2ec1a698a13f3c64b013207a75c54f8f0729e
taruvar-mittal <mittaltaruvar@gmail.com>
2022-01-09 11:32:58 +0530

    deploy

--- app.js
+++ app.js
@@ -123,6 +123,8 @@ app.use((err,req,res,next) => {
     res.status(statusCode).render('error', { err } );
 })
 
-app.listen(3000,() => {
-    console.log('Server listening on port 3000');
+const port = process.env.PORT || 3000
+
+app.listen(port,() => {
+    console.log(`Server listening on port ${port}`);
 })
