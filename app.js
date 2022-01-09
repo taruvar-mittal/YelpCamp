81b083ae547da827e172c148461ef56ac79d2b9d
taruvar-mittal <mittaltaruvar@gmail.com>
2022-01-09 11:41:52 +0530

    deploy

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const flash = require('connect-flash');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
-const MongoDbStore = require('connect-mongo');
+const MongoStore = require('connect-mongo');
 // const helmet = require('helmet');
 
 const expressMongoSanitize = require('express-mongo-sanitize');
